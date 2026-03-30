8e4475c29d291111764b0cccbbd4bef8ba092532
jayakumar09 <jayakumar.vtcc@mail.com>
2026-03-30 16:54:07 +0530

    Remove unused index.js entry point

--- index.js
+++
@@ -1,8 +0,0 @@
-// Render deployment entry point - redirects to backend server.js
-const path = require('path');
-
-// Change to backend directory
-process.chdir(path.join(__dirname, 'backend'));
-
-// Require the actual server
-require('./backend/server.js');
